8eee86ee5b5bceb5ef3fd09027bf938e9d877443
Gabriel Lopes costa <gabrielpuneco@gmail.com>
2022-04-17 11:21:38 -0300

    telas

---
+++ cadastro.js
@@ -0,0 +1,21 @@
+    let email = document.querySelector("#email");
+    let valideEmail=false;
+
+    let senha = document.querySelector("#senha");
+    let valideSenha=false;
+
+    let nome_usuario=document.querySelector("nome-usuario")
+    let valideNomeUsuariio=false;
+
+    let conf_senha=document.querySelector("conf-senha")
+    let valideConfirmSenha=false;
+
+    let msgError=document.querySelector("#msgError")
+    let msgSucess=document.querySelector("#msgSucess")
+
+
+function Cadastro(){
+  if(valideEmail && valideNomeUsuariio && valideSenha & valideConfirmSenha ){
+      
+}
+}
