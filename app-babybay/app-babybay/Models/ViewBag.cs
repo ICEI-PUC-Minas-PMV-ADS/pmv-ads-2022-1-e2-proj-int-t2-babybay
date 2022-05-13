152ac6e1f412eb942c0086a9a7c6785bf8751ed4
Gabriel Lopes costa <gabrielpuneco@gmail.com>
2022-05-13 13:13:57 -0300

    Inclusão da viewbag

--- app-babybay/app-babybay/Models/ViewBag.cs
+++ app-babybay/app-babybay/Models/ViewBag.cs
@@ -1,14 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace app_babybay.Models
+﻿namespace app_babybay.Models
 {
     // Testando layout conjunto com models usuario e produto (para cadastrar o produto a partir do usuario)
     // Sem sucesso
-    [NotMapped]
-    public class MasterLayoutViewModel
+
+
+
+    public class ViewBag
     {
+
         public Usuario Usuario { get; set; }
         public Produto Produto { get; set; }
+
+
+
         //public ICollections<Produtos> Produto { get; set; }
     }
 }
